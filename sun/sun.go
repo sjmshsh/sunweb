fb3785ab0ce6d8a116a8ba680f309a5549ee1bff
wuyan-dushang <1550693033@qq.com>
2023-04-13 20:01:34 +0800

    整合前缀树

--- sun/sun.go
+++ sun/sun.go
@@ -1,30 +1,25 @@
 package sun
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 // HandlerFunc defines the request handler used by gee
-type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+type HandlerFunc func(c *Context)
 
 // Engine implement the interface of ServeHTTP
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
@@ -40,7 +35,6 @@ func (engine *Engine) GET(pattern string, handler HandlerFunc) {
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
 	log.Printf("Route %4s - %s", method, pattern)
-	engine.router[key] = handler
+	engine.router.addRoute(method, pattern, handler)
 }
